85c8bf1a029b0aaa7ff563d09d274a7cd184993f
ornfelt <jonasornfelt@outlook.com>
2024-12-13 17:39:31 +0100

    dwm-focusonclick-20200110-61bb8b2.diff

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static char font[]            = "monospace:size=10";
 static char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { font };
