e28e5e49a7423ce0b24ffa07210c1d9e998890c9
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-09-24 07:04:01 +0200

    focusstack: honour lockfullscreen
    The stacker version of focusstack() dropped upstream's lockfullscreen
    check, so focus could move away from a fullscreen window. Restore it and
    add lockfullscreen to config.h (config.def.h already has it).

--- dwm.c
+++ dwm.c
@@ -1139,7 +1139,7 @@ focusstack(const Arg *arg)
 	int i = stackpos(arg);
 	Client *c, *p;
 
-	if(i < 0)
+	if(i < 0 || (selmon->sel && selmon->sel->isfullscreen && lockfullscreen))
 		return;
 
 	for(p = NULL, c = selmon->clients; c && (i || !ISVISIBLE(c));
