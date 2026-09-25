81b4fb457383159bf33be73cc27638af10bd6e6c
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-09-25 08:25:42 +0200

    config: pass the dmenucmd font as a separate argument
    "-fn JetBrainsMono ..." was one argv entry, which dmenu rejects as an
    unknown option.

--- config.h
+++ config.h
@@ -140,7 +140,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *termcmd[]  = { TERMINAL, NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn JetBrainsMono Nerd Font:size=11:style=bold", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", "JetBrainsMono Nerd Font:size=11:style=bold", NULL };
 
 /*
  * Xresources preferences to load at startup
